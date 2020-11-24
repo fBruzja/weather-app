4497cb0b779aa8cd71b3f2636cc5b5eb3245c503
Franc <francbruzja@gmail.com>
2020-11-24 15:20:46 +0100

    refacor cities in a separate file

--- socket-client/src/App.js
+++ socket-client/src/App.js
@@ -12,6 +12,7 @@ import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
 import Divider from "@material-ui/core/Divider";
 
+import { cities } from "./utils/cities";
 import "./App.css";
 
 const useStyles = makeStyles((theme) => ({
@@ -30,17 +31,6 @@ const useStyles = makeStyles((theme) => ({
 
 let socket = null;
 const endpoint = "http://127.0.0.1:4001";
-
-const options = [
-  { city: "Berlin", lat: "52.516667", lng: "13.4" },
-  { city: "Stuttgart", lat: "48.782343", lng: "9.180819" },
-  { city: "Frankfurt", lat: "50.11552", lng: "9.180819" },
-  { city: "Hamburg", lat: "53.575323", lng: "8.684167" },
-  { city: "Essen", lat: "51.45657", lng: "7.012282" },
-  { city: "Munich", lat: "48.15", lng: "11.583333" },
-  { city: "Cologne", lat: "50.933333", lng: "6.95" },
-  { city: "Bremen", lat: "53.073789", lng: "8.826754" },
-];
 
 export default function App() {
   const classes = useStyles();
@@ -124,13 +114,13 @@ export default function App() {
               open={Boolean(anchorEl)}
               onClose={handleClose}
             >
-              {options.map((option, index) => (
+              {cities.map((city, index) => (
                 <MenuItem
-                  key={option.city}
+                  key={city.city}
                   selected={index === selectedIndex}
-                  onClick={() => handleMenuItemClick(index, option)}
+                  onClick={() => handleMenuItemClick(index, city)}
                 >
-                  {option.city}
+                  {city.city}
                 </MenuItem>
               ))}
             </Menu>
---
+++ socket-client/src/utils/cities.js
@@ -0,0 +1,10 @@
+export const cities = [
+  { city: "Berlin", lat: "52.516667", lng: "13.4" },
+  { city: "Stuttgart", lat: "48.782343", lng: "9.180819" },
+  { city: "Frankfurt", lat: "50.11552", lng: "9.180819" },
+  { city: "Hamburg", lat: "53.575323", lng: "8.684167" },
+  { city: "Essen", lat: "51.45657", lng: "7.012282" },
+  { city: "Munich", lat: "48.15", lng: "11.583333" },
+  { city: "Cologne", lat: "50.933333", lng: "6.95" },
+  { city: "Bremen", lat: "53.073789", lng: "8.826754" },
+];
